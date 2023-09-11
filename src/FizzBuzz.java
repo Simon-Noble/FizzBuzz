9ff75684f9151a27e7c65c395d9a6cb189c44fa8
Simon Noble <expectedb@outlook.com>
2023-09-11 14:03:36 -0400

    Made doFizzBuzz public

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,12 +8,12 @@ class FizzBuzz {
         System.out.println("test");
         while(i < 100) {
 
-            doFizBuzz(i);
+            doFizzBuzz(i);
             i ++;
         }
     }
 
-    private static void doFizBuzz(int i) {
+    public static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
